deab72588ddeeab0c18712d2cb4f78da5b65d8c4
Subr1ata <subrotomukherjee8@gmail.com>
2023-11-25 17:24:41 +0530

    Light and Dark mode setup

---
+++ src/components/landing-page/title-section.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface TitleSectionProps {
+  title: string;
+  subheading?: string;
+  pill: string;
+}
+
+const TitleSection: React.FC<TitleSectionProps> = () => {
+  return <div>TitleSection</div>;
+};
+
+export default TitleSection;
