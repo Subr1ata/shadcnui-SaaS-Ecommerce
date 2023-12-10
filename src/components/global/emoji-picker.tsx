f9304f9137f354842618d325f071eee1c924bc81
Subr1ata <subrotomukherjee8@gmail.com>
2023-12-10 13:27:28 +0530

    setting emoji picker

--- src/components/global/emoji-picker.tsx
+++ src/components/global/emoji-picker.tsx
@@ -1,12 +1,34 @@
+"use client";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 interface EmojiPickerProps {
   children: React.ReactNode;
   getValue?: (emoji: string) => void;
 }
 
-const EmojiPicker = () => {
-  return <div>EmojiPicker</div>;
+interface ISelectedEmoji {
+  emoji: string;
+}
+
+const EmojiPicker: React.FC<EmojiPickerProps> = ({ children, getValue }) => {
+  const route = useRouter();
+  const Picker = dynamic(() => import("emoji-picker-react"));
+  const onClick = (selectedEmoji: ISelectedEmoji) => {
+    if (getValue) getValue(selectedEmoji.emoji);
+  };
+  return (
+    <div className="flex items-center">
+      <Popover>
+        <PopoverTrigger className="cursor-pointer">{children}</PopoverTrigger>
+        <PopoverContent className="p-0 border-none">
+          <Picker onEmojiClick={onClick} />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
 };
 
 export default EmojiPicker;
